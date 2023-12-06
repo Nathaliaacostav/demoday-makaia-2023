d34e04be8431a8dad4be6050399bf0e55bf4319c
Nathalia Acosta Vélez <nathalia285@gmail.com>
2023-12-06 00:08:32 -0500

    firebase config

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPJU_HRwaOzajcblmj52Jp8CIofLYhIrU",
+  authDomain: "demoday-makaia-2023.firebaseapp.com",
+  projectId: "demoday-makaia-2023",
+  storageBucket: "demoday-makaia-2023.appspot.com",
+  messagingSenderId: "559698518794",
+  appId: "1:559698518794:web:ea411e5ee96094942310f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
